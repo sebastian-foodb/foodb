ea884cd677fdd3025ea89fba3de057a5b76af834
Austin <f16cdude@gmail.com>
2019-11-03 17:39:40 -0500

    Updated, almost ready to parse massive CSV.

--- API_TEST/ParseNut.java
+++ API_TEST/ParseNut.java
@@ -1,32 +1,61 @@
 import org.json.*;
 
 public class ParseNut {
-	
+
 	private JsonHttpApi request;
 	private JSONObject object;
-	
+	private JSONArray arr;
+
 	public ParseNut(String fdcID) {
-		
+
 		request = new JsonHttpApi(fdcID);
 		object = new JSONObject(request.getString());
 	}
-	
+
 	public String getNut(String obj) {
-		
+
 		object.getJSONObject("foodNutrients").getString(obj);
 		return null;
-		
+
 	}
-	
+
 	public String getText() {
 		return object.toString();
 	}
-	
+
 	public void printJsonArray() {
-		JSONArray arr = object.getJSONArray("foodNutrients");
+		arr = object.getJSONArray("foodNutrients");
 		for (int i = 0; i < arr.length(); i++) {
 			System.out.println(arr.get(i));
 		}
+	}
+
+	public String[] parseJsonArray() {
+		arr = object.getJSONArray("foodNutrients");
+		String[] ret;
+		for (int i = 0; i < arr.length(); i++) {
+			
+			// obj is the working JSON Array index since the foodNutrients JSONArray
+			// is an array of json objects.
+			JSONObject obj = arr.getJSONObject(i);
+			
+			// nutrient is the nutrient object information for the particular json index.
+			JSONObject nutrient = obj.getJSONObject("nutrient");
+			
+			// self-explanatory: extracts name, amount, and the units from the nutrient JSON object.
+			String amount = String.valueOf(nutrient.getFloat("number"));
+			String name = nutrient.getString("name");
+			String unitName = nutrient.getString("unitName");
+			
+		}
+		
+		// TODO: replace and finish class.
+		return null;
+	}
+
+	public JSONArray getJsonArray() {
+		arr = object.getJSONArray("foodNutrients");
+		return arr;
 	}
 
 }
--- API_TEST/TestNut.java
+++ API_TEST/TestNut.java
@@ -1,3 +1,8 @@
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /*
  * This is a test class demonstrating how the food.csv will be parsed.
  */
@@ -9,14 +14,24 @@ public class TestNut {
 		ParseNut nut;
 		
 		//The test string is created, filled with some random FDC IDs.
-		String[] test = {"341471", "172664", "167698"};
+		//String[] test = {"341471", "172664", "167698"};
+		String [] test = {"341471"};
+		
+		JSONArray array;
 		
 		// as parsenut object is initialized with each fdc id. This does an API request and
 		// converts the json data to java objects
 		for(int i = 0; i < test.length; i++) {
 			
 			nut = new ParseNut(test[i]);
-			nut.printJsonArray();
+			//nut.printJsonArray();
+			array = nut.getJsonArray();
+			System.out.println(array.get(0));
+			JSONObject obj = array.getJSONObject(0);
+			JSONObject nutrient = obj.getJSONObject("nutrient");
+			//System.out.println(obj.get("nutrient"));
+			String amount = String.valueOf(obj.getFloat("amount"));
+			
 			
 		}
 	}
